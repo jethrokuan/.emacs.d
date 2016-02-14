182129f9a96e71e7591f2d995f47484e0ad0e4c3
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-14 21:18:37 +0800

    Make beacon color white

--- init.el
+++ init.el
@@ -194,7 +194,7 @@
   :config (progn
             (beacon-mode 1)
             (setq beacon-push-mark 35)
-            (setq beacon-color "#666600")))
+            (setq beacon-color "#FFF")))
 
 (when (window-system)
   (set-default-font "Fira Code"))
