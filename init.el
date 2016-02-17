00a9215c8b841c545bd58260f1ec867913f97010
Jethro Kuan <jethrokuan95@gmail.com>
2016-02-17 21:31:38 +0800

    Removed custom-set-vars

--- init.el
+++ init.el
@@ -593,17 +593,3 @@
 	       (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-optionss)))
 (provide 'init.el)
 ;;init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/.org/books.org" "~/.org/concurrency-with-core-async.org" "~/.org/ideas.org" "~/.org/links.org" "~/.org/personal.org" "~/.org/someday.org" "~/.org/temp vocab.org" "~/.org/today.org" "~/.org/vocab.org"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
