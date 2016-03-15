9d3de3b318efa075bd7f0e60404df5924295e2f3
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-15 14:06:26 +0800

    Treat smart-mode-line theme as safe

--- init.el
+++ init.el
@@ -539,3 +539,17 @@
 
 (provide 'init.el)
 ;;init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
