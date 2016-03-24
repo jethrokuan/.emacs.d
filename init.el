be58467a09f6e2b8ba57e4e37d2fb540300c75de
Jethro Kuan <jethrokuan95@gmail.com>
2016-03-24 21:07:59 +0800

    Purge old backup files

--- init.el
+++ init.el
@@ -50,7 +50,21 @@
 
 ;;;; Changes for sanity
 ;; Change backup directory to prevent littering of working dir
-(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;;; Purge old backup files
+(message "Deleting old backup files...")
+(let ((week (* 60 60 24 7))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
 
 ;;Keybindings
 (bind-key* "C-x m" 'eshell)
