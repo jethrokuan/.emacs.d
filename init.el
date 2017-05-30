8a15fcfb02c9a1e796e04df5defa35fdc9e97952
Jethro Kuan <jethrokuan95@gmail.com>
2017-05-30 21:57:33 +0800

    Move emacs config to own repo

---
+++ init.el
@@ -0,0 +1,8 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+
+(require 'org)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
