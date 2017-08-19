59836eba1aa52737115bc40d5992ce7a35d5a9ce
Jethro Kuan <jethrokuan95@gmail.com>
2017-08-19 17:03:12 +0800

    remove custom face

--- custom.el
+++ custom.el
@@ -55,6 +55,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Iosevka"))))
- '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B")))))
+ '(fixed-pitch ((t (:family "Iosevka")))) 
  '(variable-pitch ((t (:family "Georgia")))))
