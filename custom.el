2916f374841a369feaf912c21feda1f491f588d9
Jethro Kuan <jethrokuan95@gmail.com>
2017-11-02 02:05:22 +0800

    Reenable smart-mode-line

--- custom.el
+++ custom.el
@@ -3,51 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(Linum-format "%7i ")
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(elfeed-feeds
-   (quote
-    ("http://slatestarcodex.com/feed/" "https://news.ycombinator.com/rss" "http://planet.emacsen.org/atom.xml")))
- '(fci-rule-color "#F1F1F1")
- '(main-line-color1 "#1E1E1E")
- '(main-line-color2 "#111111")
- '(main-line-separator-style (quote chamfer))
- '(nrepl-message-colors
-   (quote
-    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
- '(org-babel-load-languages (quote ((shell . t) (emacs-lisp . t))))
- '(org-fontify-done-headline t)
- '(org-fontify-quote-and-verse-blocks t)
- '(org-fontify-whole-heading-line t)
  '(package-selected-packages
    (quote
-    (company-go gorepl-mode golint go-dlv challenger-deep pdf-view magithub all-the-icons-dired typescript-mode alchemist elixir-mode tide yaml-mode intero shm isend-mode link-hint eww-lnum highlight-escape-sequences highlight-operators highlight-defined highlight-quoted highlight-numbers rainbow-delimiters color-identifiers-mode color-theme-sanityinc-tomorrow afternoon-theme darktooth-theme soothe-theme elfeed doom-themes 0blayout edbi-database-url edbi-django edbi eshell-git-prompt eshell-git-prompt-use-theme rjsx-mode peep-dired no-littering org-with-contrib shackle ibuffer-projectile diff-hl dumb-jump indium realgud easy-kill conkeror-minor-mode key-chord tao-theme fancy-narrow guru-mode slack flx-isearch sos nyan-mode evil-surround evil evil-mode highlight-indent-guides flycheck-color-mode-line smart-mode-line zenburn-theme yapfify which-key web-mode vue-mode volatile-highlights visual-regexp validate use-package spotify sphinx-doc smartparens slime-company skewer-mode scss-mode rust-mode rainbow-mode pytest py-isort py-autopep8 pdf-tools paradox page-break-lines ox-reveal org-plus-contrib org-download org-alert notmuch nix-mode nim-mode nameless move-text markdown-mode magit keyfreq json-mode js2-refactor js-doc iedit htmlize golden-ratio go-mode git-gutter-fringe+ gist focus flycheck-pos-tip flycheck-clojure flx fish-mode firestarter exwm expand-region exec-path-from-shell esup epresent emmet-mode elpy edit-indirect direnv dired-ranger dired-narrow dired-k deft darkroom crux counsel-projectile company-tern company-quickhelp company-nixos-options company-auctex company-anaconda clj-refactor bury-successful-compilation beacon base16-theme avy artbollocks-mode anzu aggressive-indent)))
- '(paradox-github-token t)
- '(vc-annotate-background "#F6F6F6")
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#C3C3C3")
-     (40 . "#9E9E9E")
-     (60 . "#9E9E9E")
-     (80 . "#616161")
-     (100 . "#616161")
-     (120 . "#3C3C3C")
-     (140 . "#3C3C3C")
-     (160 . "#252525")
-     (180 . "#252525")
-     (200 . "#252525")
-     (220 . "#171717")
-     (240 . "#171717")
-     (260 . "#171717")
-     (280 . "#0E0E0E")
-     (300 . "#0E0E0E")
-     (320 . "#0E0E0E")
-     (340 . "#090909")
-     (360 . "#090909"))))
- '(vc-annotate-very-old-color "#3C3C3C"))
+    (challenger-deep zenburn-theme yapfify yaml-mode ws-butler which-key wgrep web-mode vue-mode volatile-highlights visual-regexp use-package tide sphinx-doc spaceline sos smartparens slime-company skewer-mode shm shackle scss-mode rust-mode rjsx-mode rg rainbow-delimiters pytest py-isort peep-dired paradox ox-ioslide org-ref org-pomodoro org-plus-contrib org-bullets olivetti no-littering nix-sandbox nix-mode move-text markdown-mode magit keyfreq js2-refactor js-doc ivy-bibtex isend-mode intero interleave indium iedit ibuffer-projectile highlight-indent-guides guru-mode golden-ratio go-mode flycheck-pos-tip flycheck-color-mode-line flycheck-clojure flx fish-mode fancy-narrow eyebrowse expand-region eww-lnum esup eshell-git-prompt emmet-mode elfeed-org easy-kill dumb-jump dockerfile-mode docker direnv dired-subtree dired-ranger dired-narrow dired-k diff-hl deft crux counsel-projectile company-tern company-quickhelp company-auctex company-anaconda clj-refactor challenger-deep-theme bury-successful-compilation avy alchemist aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
