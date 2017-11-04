3f9175bc2f46e6d02df8fbf084af996927ec213d
Jethro Kuan <jethrokuan95@gmail.com>
2017-11-04 20:29:16 +0800

    (mode-line): Add thick modeline customization

--- custom.el
+++ custom.el
@@ -14,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B"))))))
