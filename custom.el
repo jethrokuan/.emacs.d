cdcf7c8952e4be85fcaeb98715bf0df12923566f
Jethro Kuan <jethrokuan95@gmail.com>
2018-01-05 02:28:10 +0800

    misc update

--- custom.el
+++ custom.el
@@ -8,8 +8,10 @@
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(package-selected-packages
    (quote
-    (org-pomodoro org-download org-bullets org-plus-contrib zenburn-theme yapfify yaml-mode ws-butler which-key wgrep web-mode vue-mode volatile-highlights visual-regexp use-package tide sphinx-doc sos smartparens smart-mode-line slime-company skewer-mode shackle scss-mode rust-mode rjsx-mode rg rainbow-mode rainbow-delimiters pytest py-isort peep-dired org-ref olivetti notmuch no-littering nix-mode markdown-mode magit js2-refactor js-doc isend-mode intero indium highlight-indent-guides guru-mode gradle-mode golden-ratio go-mode flycheck-pos-tip flycheck-clojure flx fish-mode eyebrowse expand-region eshell-git-prompt ensime emmet-mode elfeed-org easy-kill dumb-jump dockerfile-mode docker direnv dired-subtree dired-ranger dired-narrow dired-k diminish diff-hl deft crux counsel-projectile company-tern company-quickhelp company-auctex company-anaconda clj-refactor bury-successful-compilation avy alert alchemist aggressive-indent)))
- '(send-mail-function (quote smtpmail-send-it)))
+    (org-pomodoro org-download org-bullets org-plus-contrib zenburn-theme yapfify yaml-mode ws-butler which-key wgrep web-mode vue-mode volatile-highlights visual-regexp use-package tide sphinx-doc sos smartparens smart-mode-line slime-company skewer-mode shackle scss-mode rust-mode rjsx-mode rg rainbow-mode rainbow-delimiters pytest py-isort peep-dired pdf-tools olivetti notmuch no-littering nix-mode markdown-mode magit key-chord js2-refactor js-doc isend-mode intero indium highlight-indent-guides helm-bibtex guru-mode gradle-mode golden-ratio go-mode flycheck-pos-tip flycheck-clojure flx fish-mode eyebrowse expand-region eshell-git-prompt ensime emmet-mode elfeed-org easy-kill dumb-jump dockerfile-mode docker direnv dired-subtree dired-ranger dired-narrow dired-k diminish diff-hl deft crux counsel-projectile company-tern company-quickhelp company-auctex company-anaconda clj-refactor bury-successful-compilation avy alert alchemist aggressive-indent)))
+ '(safe-local-variable-values
+   (quote
+    ((org-download-image-dir . "./images/operating_systems/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
