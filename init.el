0a032807ef1b0f9993a7481185548b3128abfc0f
Jethro Kuan <jethrokuan95@gmail.com>
2018-01-06 17:19:44 +0800

    update

--- init.el
+++ init.el
@@ -1,10 +1,7 @@
-;; (require 'package)
-
+(require 'package)
 ;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-;; (package-initialize)
+(package-initialize)
 
-;; (require 'org)
-;; (require 'ob-tangle)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (put 'dired-find-alternate-file 'disabled nil)
