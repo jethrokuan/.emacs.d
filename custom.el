ff78671c6cf81e0d24d215d35a43a732e7e0813e
Jethro Kuan <jethrokuan95@gmail.com>
2018-01-17 01:23:29 +0800

    update safe variables

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(safe-local-variable-values
+   (quote
+    ((org-download-image-dir . "./images/operating_systems/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
