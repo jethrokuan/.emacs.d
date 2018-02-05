c0522246553bfe0beed6aad0ac7d5d2a9d2e7824
Jethro Kuan <jethrokuan95@gmail.com>
2018-02-05 11:05:07 +0800

    add custom

--- custom.el
+++ custom.el
@@ -5,16 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(safe-local-variable-values
-   (quote
-    ((org-download-image-dir . "./images/ai/")
-     (org-download-image-dir . "./images/nlp/")
-     (org-download-image-dir . "./images/operating_systems/")))))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B")))))
- '(variable-pitch ((t (:family "DejaVu Sans Mono 12")))))
+ '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B"))))))
