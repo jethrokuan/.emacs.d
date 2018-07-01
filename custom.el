f6c3eddfdf956392c9835f760456ecf974e48b81
Jethro Kuan <jethrokuan95@gmail.com>
2018-07-01 21:34:27 +0800

    Add org-mode ricing

--- custom.el
+++ custom.el
@@ -5,10 +5,16 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B"))))))
+ '(cursor ((t (:background "dim gray"))))
+ '(org-document-title ((t (:foreground "#171717" :weight bold :height 1.5))))
+ '(org-headline-done ((t (:foreground "#171717" :strike-through t))))
+ '(org-level-1 ((t (:foreground "#090909" :weight bold :height 1.3))))
+ '(org-level-2 ((t (:foreground "#090909" :weight normal :height 1.2))))
+ '(org-level-3 ((t (:foreground "#090909" :weight normal :height 1.1))))
+ '(variable-pitch ((t (:background "#F1F1F1" :foreground "#616161" :family "ETBembo")))))
