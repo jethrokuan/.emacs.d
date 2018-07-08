b9a84c2c7fc330436cd7c59f01bb9880eb01d248
Jethro Kuan <jethrokuan95@gmail.com>
2018-07-08 20:03:20 +0800

    Use password-store for passwords
    - also, remove emacs lockfiles

--- custom.el
+++ custom.el
@@ -5,13 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(safe-local-variable-values
+   (quote
+    ((org-download-image-dir . "./images/ai/")
+     (org-download-image-dir . "./images/nlp/")
+     (org-download-image-dir . "./images/operating_systems/")
+     (org-download-image-dir . "./images/software-engineering/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "dim gray"))))
  '(org-document-title ((t (:foreground "#171717" :weight bold :height 1.5))))
  '(org-done ((t (:background "#E8E8E8" :foreground "#0E0E0E" :strike-through t :weight bold))))
  '(org-headline-done ((t (:foreground "#171717" :strike-through t))))
