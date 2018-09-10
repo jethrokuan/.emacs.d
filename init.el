58b8da96225f5c471976823f68e6705a6089d6d7
Jethro Kuan <jethrokuan95@gmail.com>
2018-09-10 11:57:52 +0800

    set package-archives only if on mac osx

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(if (memq window-system '(mac ns))
+    (progn
+      (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+      (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
+  (setq package-archives nil))
+
 (package-initialize)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
